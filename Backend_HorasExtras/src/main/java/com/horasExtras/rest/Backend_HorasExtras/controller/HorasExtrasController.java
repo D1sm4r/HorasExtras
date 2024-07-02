1cf179fd12c47b0fb39601a71d2af2e6b7e9dead
javier.choque <jchoque0@gmail.com>
2024-07-02 00:42:37 -0400

    Monolitico

--- Backend_HorasExtras/src/main/java/com/horasExtras/rest/Backend_HorasExtras/controller/HorasExtrasController.java
+++ Backend_HorasExtras/src/main/java/com/horasExtras/rest/Backend_HorasExtras/controller/HorasExtrasController.java
@@ -1,15 +1,19 @@
-package com.horasExtras.rest.Backend_HorasExtras.controller;
+package com.horasExtras.rest.Backend_HorasExtras.dto.controller;
 
 import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
 
+import com.horasExtras.rest.Backend_HorasExtras.service.INotificacionService;
+import com.horasExtras.rest.Backend_HorasExtras.service.IProyectoService;
+import com.horasExtras.rest.Backend_HorasExtras.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import com.horasExtras.rest.Backend_HorasExtras.dto.HorasExtrasDTO;
@@ -22,6 +26,14 @@ public class HorasExtrasController {
     @Autowired
     private IHorasExtrasService servicio;
 
+    @Autowired
+    private IProyectoService bdproyecto;
+
+    @Autowired
+    private IUserService bduser;
+
+    @Autowired
+    private INotificacionService bdnotificaciones;
 
     @PostMapping("solicitar")
     public ResponseEntity<HorasExtrasDTO> solicitarHorasExtras(@RequestBody HorasExtrasDTO horasExtrasDTO) {
@@ -31,10 +43,10 @@ public class HorasExtrasController {
 
     @ResponseBody
     @PostMapping("create")
-    public HorasExtrasDTO agregarHE(@Valid @NonNull @RequestBody HorasExtrasDTO dto) {
-
+    public HorasExtrasDTO agregarHE(@ModelAttribute HorasExtrasDTO dto) {
         return servicio.save(dto);
     }
+
 
     @ResponseBody
     @GetMapping("findall")
@@ -77,4 +89,40 @@ public class HorasExtrasController {
             return false;
         }
     }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // FRONTEND ///////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////
+
+    @GetMapping("listar/REST")
+    public String listarREST(Model model) {
+        List<HorasExtrasDTO> hs = servicio.findAll();
+        HorasExtrasDTO horasExtrasDTO = new HorasExtrasDTO();
+        model.addAttribute("horaextra",horasExtrasDTO);
+        model.addAttribute("hs", hs);
+        model.addAttribute("proyectos",bdproyecto.findAll());
+        model.addAttribute("usuarios",bduser.findAll());
+        model.addAttribute("notificaciones",bdnotificaciones.findAll());
+        return "rest/index";
+    }
+
+    // http://localhost:8081/horaextras/listar/nuevo/REST
+    @GetMapping("listar/nuevo/REST")
+    public String agregarREST(Model model) {
+        model.addAttribute("usuarios",bduser.findAll());
+        model.addAttribute("proyectos",bdproyecto.findAll());
+        model.addAttribute("horaextra", new HorasExtrasDTO());
+        return "rest/form";
+    }
+
+    // http://localhost:8081/horaextras/REST/id
+    @GetMapping("editar/REST/{id}")
+    public String editarREST(@PathVariable long id, Model model) {
+        Optional<HorasExtrasDTO> horaextra = servicio.findById(id);
+        model.addAttribute("horaextra", horaextra);
+        model.addAttribute("proyectos", bdproyecto.findAll());
+        model.addAttribute("usuarios", bduser.findAll());
+        return "rest/form";
+    }
+
 }
